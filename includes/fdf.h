2668e72b49648007cc248fbf5d40f157100f5401
Florian Varrin <fvarrin@student.42lausanne.ch>
2021-12-08 15:59:59 +0100

    Add window init and close

--- includes/fdf.h
+++ includes/fdf.h
@@ -6,11 +6,21 @@
 /*   By: fvarrin <florian.varrin@gmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/06 11:53:38 by fvarrin           #+#    #+#             */
-/*   Updated: 2021/12/06 11:54:14 by fvarrin          ###   ########.fr       */
+/*   Updated: 2021/12/08 15:57:33 by fvarrin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FDF_H
 # define FDF_H
+
+# define WINDOW_WIDTH 1920
+# define WINDOW_HEIGHT 1080
+# define WINDOW_NAME "FdF"
+
+typedef struct	s_window {
+	void	*mlx;
+	void	*win;
+	int		fd;
+}	t_window;
 
 #endif
--- srcs/main.c
+++ srcs/main.c
@@ -6,15 +6,58 @@
 /*   By: fvarrin <florian.varrin@gmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/06 11:52:17 by fvarrin           #+#    #+#             */
-/*   Updated: 2021/12/08 12:49:28 by fvarrin          ###   ########.fr       */
+/*   Updated: 2021/12/08 15:59:10 by fvarrin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+#include "fdf.h"
+
 #include <mlx.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
 
-int	main(void)
+void    print_usage(void)
 {
-	void	*mlx;
+        ft_printf("usage: ./fdf [path-to-map]\n");
+        exit(-1);
+}
 
-	mlx = mlx_init();
+void	exit_program_gracefully(t_window *window)
+{
+	close(window->fd);
+	exit(0);
+}
+
+int	handle_key(int keycode, t_window *window)
+{
+	if (window == NULL)
+		return (0);
+	if (keycode == 53)
+		exit_program_gracefully(window);
+	return (0);
+}
+
+void	init_window(t_window *window, int fd)
+{
+	window->mlx = mlx_init();
+	window->win = mlx_new_window(window->mlx, WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_NAME);
+	window->fd = fd;
+	mlx_key_hook(window->win, handle_key, &window);
+}
+
+int	main(int argc, char **argv)
+{
+	int			fd;
+	t_window	window;
+
+	if (argc != 2)
+		print_usage();
+	fd = open(argv[1], O_RDONLY);
+	if (fd < 0)
+		print_usage();
+	init_window(&window, fd);
+	mlx_loop(window.mlx);
+	exit_program_gracefully(&window);
 }
