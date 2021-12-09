6f3e4d2f82cbc4db5077327efc558eb1e1ac73c8
Florian Varrin <fvarrin@student.42lausanne.ch>
2021-12-09 17:12:44 +0100

    [WIP] exploring

--- includes/fdf.h
+++ includes/fdf.h
@@ -6,16 +6,17 @@
 /*   By: fvarrin <florian.varrin@gmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/06 11:53:38 by fvarrin           #+#    #+#             */
-/*   Updated: 2021/12/08 15:57:33 by fvarrin          ###   ########.fr       */
+/*   Updated: 2021/12/09 10:15:11 by fvarrin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FDF_H
 # define FDF_H
 
-# define WINDOW_WIDTH 1920
-# define WINDOW_HEIGHT 1080
+# define WINDOW_WIDTH 700
+# define WINDOW_HEIGHT 700
 # define WINDOW_NAME "FdF"
+# define COLOR 0x00FF0000
 
 typedef struct	s_window {
 	void	*mlx;
@@ -23,4 +24,11 @@ typedef struct	s_window {
 	int		fd;
 }	t_window;
 
+typedef struct	s_image {
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}				t_image;
 #endif
--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: fvarrin <florian.varrin@gmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/06 11:52:17 by fvarrin           #+#    #+#             */
-/*   Updated: 2021/12/08 15:59:10 by fvarrin          ###   ########.fr       */
+/*   Updated: 2021/12/09 11:26:37 by fvarrin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,6 +39,12 @@ int	handle_key(int keycode, t_window *window)
 	return (0);
 }
 
+void	init_image(t_window *window, t_image *image)
+{
+	image->img = mlx_new_image(window->mlx, WINDOW_WIDTH, WINDOW_HEIGHT);
+	image->addr = mlx_get_data_addr(image->img, &(image->bits_per_pixel), &(image->line_length), &(image->endian));
+}
+
 void	init_window(t_window *window, int fd)
 {
 	window->mlx = mlx_init();
@@ -47,10 +53,53 @@ void	init_window(t_window *window, int fd)
 	mlx_key_hook(window->win, handle_key, &window);
 }
 
+void	put_pixel_to_image(t_image *image, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = image->addr + (y * image->line_length + x * (image->bits_per_pixel / 8));
+	*(unsigned int*)dst = color;
+}
+
+void drawline(t_image *image, int x0, int x1, int y0, int y1)
+{
+	int dx;
+	int dy;
+	int p;
+	int x;
+	int	y;
+
+	dx = x1 - x0;
+	dy = y1 - y0;
+ 	x = x0;
+	y = y0;
+ 	p = 2 * dy - dx;
+ 
+	ft_printf("x: %d, x1: %d", x, x1);
+	while(x < x1)
+	{
+		if(p >= 0)
+		{
+			ft_printf("(%d, %d)\n", x, y);
+			put_pixel_to_image(image, x, y, COLOR);
+			y = y + 1;
+			p = p + 2 * dy - 2 * dx;
+		}
+		else
+		{
+			ft_printf("(%d, %d)\n", x, y);
+			put_pixel_to_image(image, x, y, COLOR);
+			p = p + 2 * dy;
+		}
+		x = x + 1;
+	}
+}
+
 int	main(int argc, char **argv)
 {
 	int			fd;
 	t_window	window;
+	t_image		image;
 
 	if (argc != 2)
 		print_usage();
@@ -58,6 +107,9 @@ int	main(int argc, char **argv)
 	if (fd < 0)
 		print_usage();
 	init_window(&window, fd);
+	init_image(&window, &image);
+	drawline(&image, 0, 0, 25, 25);
+	mlx_put_image_to_window(window.mlx, window.win, image.img, 0, 0);
 	mlx_loop(window.mlx);
 	exit_program_gracefully(&window);
 }
