9055b05641624421abad67001c943f1dae12d3a6
fvarrin <florian.varrin@gmail.com>
2022-02-16 10:57:51 +0100

    Update libs and clean main

--- srcs/main.c
+++ srcs/main.c
@@ -6,17 +6,14 @@
 /*   By: fvarrin <florian.varrin@gmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/06 11:52:17 by fvarrin           #+#    #+#             */
-/*   Updated: 2022/01/08 15:37:25 by fvarrin          ###   ########.fr       */
+/*   Updated: 2022/02/03 10:36:06 by fvarrin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include "fdf.h"
-#include "mlx-wrapper.h"
+#include "mlx.h"
 
 #include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
 
 void    print_usage(void)
 {
